dd6eef8418f3606e0946a12d958e159a3507596c
safiraazizah2706-glitch <safiraazizah2706@gmail.com>
2025-10-27 01:02:18 +0700

    tugas php

---
+++ php/konstanta.php
@@ -0,0 +1,8 @@
+<?php
+define("NAMA", Muhammad Encep");
+define("NILAI", 90);
+
+echo "---Belajar Konstanta ---<br><br>";
+echo "nama :" . NAMA;
+echo "<br>Nilai :". Nilai;
+?>
---
+++ php/variabel.php
@@ -0,0 +1,18 @@
+<?php
+$char = 'A';
+$string = Muhammad Encep";
+$a = 36;
+$penghasilan = 1000000;
+$nilai_uts =n82.25;
+
+echo "---Belajar Variabel --- <br><br>";
+echo "Variabel Char : " . $char ."<br>";
+echo "Variabel String : " . $string<br>";
+echo "Variabel a : $a<br>";
+echo "Variabel penghasilan1 : $penghasilan1<br>";
+echo "Variabel nilai_uts : $nilai_uts<br>";
+?>
+
+
+
+
